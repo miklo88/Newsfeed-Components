272a283dc4a33bacd63113490fba140a47639d37
Carlos <miklo.concepcion@gmail.com>
2019-08-13 10:22:16 -0500

    first commit. Creating new elements and their attributes. Located in console not on browser yet.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -12,7 +12,6 @@ let menuItems = [
 /* 
 
   Step 1: Write a function that will create a menu component as seen below:
-
   <div class="menu">
     <ul>
       {each menu item as a list item}
@@ -22,7 +21,7 @@ let menuItems = [
   Pass the function an array as it's only argument.
 
   Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
+  Add those items to the <ul> .map
 
   Step 3: Using a DOM selector, select the menu button currently on the DOM.
   
